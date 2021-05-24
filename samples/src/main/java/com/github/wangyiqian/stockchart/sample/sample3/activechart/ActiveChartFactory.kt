ff21b52d95cef552fe460a18fae0802ddb25c78b
wangyiqian <wangyiqian9891@gmail.com>
2021-05-24 14:29:47 +0800

    新增大盘异动示例
    This reverts commit aa93d67b1ef4f6b3f735744fbdf9832b2c696b0f.

---
+++ samples/src/main/java/com/github/wangyiqian/stockchart/sample/sample3/activechart/ActiveChartFactory.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2021 WangYiqian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package com.github.wangyiqian.stockchart.sample.sample3.activechart
+
+import com.github.wangyiqian.stockchart.IStockChart
+import com.github.wangyiqian.stockchart.childchart.base.AbsChildChartFactory
+
+/**
+ * @author wangyiqian E-mail: wangyiqian9891@gmail.com
+ * @version 创建时间: 2021/5/14
+ */
+class ActiveChartFactory(stockChart: IStockChart, childChartConfig: ActiveChartConfig) :
+    AbsChildChartFactory<ActiveChartConfig>(stockChart, childChartConfig) {
+
+    override fun createChart() =
+        ActiveChart(stockChart, childChartConfig)
+}
