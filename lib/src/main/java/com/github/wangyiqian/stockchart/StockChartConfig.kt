752222261684cc5c720fb0c90cb8093f30bd9500
wangyiqian <wangyiqian9891@gmail.com>
2022-06-17 18:59:45 +0800

    增加一个高级用法配置extMatrix

--- lib/src/main/java/com/github/wangyiqian/stockchart/StockChartConfig.kt
+++ lib/src/main/java/com/github/wangyiqian/stockchart/StockChartConfig.kt
@@ -13,7 +13,7 @@
 
 package com.github.wangyiqian.stockchart
 
-import android.graphics.DashPathEffect
+import android.graphics.Matrix
 import android.graphics.PathEffect
 import androidx.annotation.ColorInt
 import com.github.wangyiqian.stockchart.childchart.base.*
@@ -27,47 +27,68 @@ import com.github.wangyiqian.stockchart.listener.OnLoadMoreListener
  */
 class StockChartConfig {
 
-    // K线数据
+    /**
+     * K线数据
+     */
     var kEntities = mutableListOf<IKEntity>()
         set(value) {
             setKEntities(value, 0, kEntities.size - 1)
         }
 
-    // 初始显示区域的起始坐标
+    /**
+     * 初始显示区域的起始坐标
+     */
     var showStartIndex = 0
 
-    // 初始显示区域的结束坐标
+    /**
+     * 初始显示区域的结束坐标
+     */
     var showEndIndex = 0
 
-    // 是否支持滑动
+    /**
+     * 是否支持滑动
+     */
     var scrollAble =
         DEFAULT_SCROLL_ABLE
 
-    // 是否支持"滑过头回弹"效果
+    /**
+     * 是否支持"滑过头回弹"效果
+     */
     var overScrollAble =
         DEFAULT_OVER_SCROLL_ABLE
 
-    // "滑过头回弹"最大距离
+    /**
+     * "滑过头回弹"最大距离
+     */
     var overScrollDistance =
         DEFAULT_OVER_SCROLL_DISTANCE
 
-    // "滑过头回弹"过程中触发加载更多需要的距离
+    /**
+     * "滑过头回弹"过程中触发加载更多需要的距离
+     */
     var overScrollOnLoadMoreDistance =
         DEFAULT_OVER_SCROLL_ON_LOAD_MORE_DISTANCE
 
-    // 是否支持双指缩放
+    /**
+     * 是否支持双指缩放
+     */
     var scaleAble =
         DEFAULT_SCALE_ABLE
 
-    // 是否需要"平滑的"滑动。如果false，滑动时一个下标对应的内容要么全显示，要么不显示。
-    var scrollSmoothly =
-        DEFAULT_SCROLL_SMOOTHLY
+    /**
+     * 是否需要"平滑的"滑动。如果false，滑动时一个下标对应的内容要么全显示，要么不显示。
+     */
+    var scrollSmoothly = DEFAULT_SCROLL_SMOOTHLY
 
-    // 超出滑动限制范围时拖动的"摩擦力"
+    /**
+     * 超出滑动限制范围时拖动的"摩擦力"
+     */
     var frictionScrollExceedLimit =
         DEFAULT_FRICTION_SCROLL_EXCEED_LIMIT
 
-    // 双指缩放最大缩放比例
+    /**
+     * 双指缩放最大缩放比例
+     */
     var scaleFactorMax =
         DEFAULT_SCALE_FACTOR_MAX
         set(value) {
@@ -75,7 +96,9 @@ class StockChartConfig {
             field = value
         }
 
-    // 双指缩放最小缩放比例
+    /**
+     * 双指缩放最小缩放比例
+     */
     var scaleFactorMin =
         DEFAULT_SCALE_FACTOR_MIN
         set(value) {
@@ -83,70 +106,106 @@ class StockChartConfig {
             field = value
         }
 
-    // 是否支持长按高亮横线
+    /**
+     * 是否支持长按高亮横线
+     */
     var showHighlightHorizontalLine =
         DEFAULT_SHOW_HIGHLIGHT_HORIZONTAL_LINE
 
-    // 长按高亮横线宽度
+    /**
+     * 长按高亮横线宽度
+     */
     var highlightHorizontalLineWidth =
         DEFAULT_HIGHLIGHT_HORIZONTAL_LINE_WIDTH
 
-    // 长按高亮横线颜色
+    /**
+     * 长按高亮横线颜色
+     */
     var highlightHorizontalLineColor =
         DEFAULT_HIGHLIGHT_HORIZONTAL_LINE_COLOR
 
-    // 是否支持长按高亮竖线
+    /**
+     * 是否支持长按高亮竖线
+     */
     var showHighlightVerticalLine =
         DEFAULT_SHOW_HIGHLIGHT_VERTICAL_LINE
 
-    // 长按高亮竖线宽度
+    /**
+     * 长按高亮竖线宽度
+     */
     var highlightVerticalLineWidth =
         DEFAULT_HIGHLIGHT_VERTICAL_LINE_WIDTH
 
-    // 长按高亮竖线颜色
+    /**
+     * 长按高亮竖线颜色
+     */
     var highlightVerticalLineColor =
         DEFAULT_HIGHLIGHT_VERTICAL_LINE_COLOR
 
-    // 涨色值
+    /**
+     * 涨色值
+     */
     var riseColor: Int =
         DEFAULT_RISE_COLOR
 
-    // 跌色值
+    /**
+     * 跌色值
+     */
     var downColor: Int =
         DEFAULT_DOWN_COLOR
 
-    // 背景色
+    /**
+     * 背景色
+     */
     @ColorInt
     var backgroundColor = DEFAULT_BACKGROUND_COUNT
 
-    // 背景网格横线数
+    /**
+     * 背景网格横线数
+     */
     var gridHorizontalLineCount = DEFAULT_GRID_HORIZONTAL_LINE_COUNT
 
-    // 背景网格竖线数
+    /**
+     * 背景网格竖线数
+     */
     var gridVerticalLineCount = DEFAULT_GRID_VERTICAL_LINE_COUNT
 
-    // 背景网格线条色
+    /**
+     * 背景网格线条色
+     */
     @ColorInt
     var gridLineColor = DEFAULT_GRID_LINE_COLOR
 
-    // 背景网格线条宽度
+    /**
+     * 背景网格线条宽度
+     */
     var gridLineStrokeWidth = DEFAULT_GRID_LINE_STROKE_WIDTH
 
-    // 背景网格线条虚线配置
+    /**
+     * 背景网格线条虚线配置
+     */
     var gridLinePathEffect: PathEffect? = null
 
-    // 背景网格横线第一条线的顶部偏移量
+    /**
+     * 背景网格横线第一条线的顶部偏移量
+     */
     var horizontalGridLineTopOffsetCalculator: ((StockChart) -> Float)? = null
 
-    // 背景网格横线左侧偏移量
+    /**
+     * 背景网格横线左侧偏移量
+     */
     var horizontalGridLineLeftOffsetCalculator: ((StockChart) -> Float)? = null
 
-    // 背景网格横线间距
+    /**
+     * 背景网格横线间距
+     */
     var horizontalGridLineSpaceCalculator: ((StockChart) -> Float)? = null
 
     var valueTendToZero = DEFAULT_VALUE_TEND_TO_ZERO
 
-    // 加载更多监听
+    /**
+     * 加载更多监听
+     */
     var onLoadMoreListener: OnLoadMoreListener? = null
         set(value) {
             field?.apply { removeOnLoadMoreListener(this) }
@@ -155,13 +214,20 @@ class StockChartConfig {
 
         }
 
-    // 手势监听
+    /**
+     * 手势监听
+     */
     var onGestureListener: OnGestureListener? = null
         set(value) {
             field?.apply { removeOnGestureListener(this) }
             value?.apply { addOnGestureListener(this) }
             field = value
         }
+
+    /**
+     * 高级用法，每个子图最终绘制追加一个Matrix
+     */
+    var extMatrix: Matrix? = null
 
     val childChartFactories = mutableListOf<AbsChildChartFactory<*>>()
 
